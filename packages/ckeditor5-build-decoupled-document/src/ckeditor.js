51001ffa60229592c984c76f0e6b750681ec7434
Carol Dysart <carol@buildops.com>
2021-01-11 09:53:11 -0800

    added restricted editing

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.js
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.js
@@ -48,6 +48,7 @@ import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak';
 import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed';
 import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
+import RestrictedEditingMode from '@ckeditor/ckeditor5-restricted-editing/src/restrictededitingmode';
 
 export default class DecoupledEditor extends DecoupledEditorBase {}
 const customColorPalette = [
@@ -122,6 +123,7 @@ DecoupledEditor.builtinPlugins = [
   HtmlEmbed,
   PageBreak,
   Pagination,
+  RestrictedEditingMode,
   TableToolbar,
   TableProperties,
   TableCellProperties
@@ -171,7 +173,8 @@ DecoupledEditor.defaultConfig = {
 
       'exportWord',
       '|',
-      'HorizontalLine'
+      'HorizontalLine',
+      'restrictedEditing'
     ]
   },
   pagination: {
